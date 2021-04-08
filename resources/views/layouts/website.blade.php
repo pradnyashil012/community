b540e42783ec4440848f174ba4aded86b9245742
pradnyashil012 <56130908+pradnyashil012@users.noreply.github.com>
2021-04-08 16:48:58 +0530

    updated website.blade.php
    Search bar position changed

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -38,15 +38,10 @@
             <div class="container-fluid">
                 <div class="row align-items-center">
 
-                    <div class="col-12 search-form-wrap js-search-form">
-                        <form method="get" action="#">
-                            <input type="text" id="s" class="form-control" placeholder="Search...">
-                            <button class="search-btn" type="submit"><span class="icon-search"></span></button>
-                        </form>
-                    </div>
+                   
 
                     <div class="col-4 site-logo">
-                        <a href="{{route('website')}}" class="text-black h2 mb-0">Mini Blog</a>
+                        <a href="{{route('website')}}" class="text-black h2 mb-0">Ipay Community</a>
                     </div>
 
                     <div class="col-8 text-right">
@@ -64,10 +59,20 @@
                         <a href="#" class="site-menu-toggle js-menu-toggle text-black d-inline-block d-lg-none"><span
                                 class="icon-menu h3"></span></a>
                     </div>
+
+                    
+
                 </div>
 
             </div>
         </header>
+
+        <div class="col-12 search-form-wrap js-search-form">
+                        <form method="get" action="#">
+                            <input type="text" id="s" class="form-control" placeholder="Search...">
+                            <button class="search-btn" type="submit"><span class="icon-search"></span></button>
+                        </form>
+                    </div>
 
         @yield('content')
 

cc401179ac45b9f50aafbebd0c7d2c6f0e705b61
pradnyashil012 <56130908+pradnyashil012@users.noreply.github.com>
2021-04-08 16:17:23 +0530

    admin changes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -50,7 +50,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('dashboard') }}" class="brand-link">
-      <img src="{{asset('admin')}}/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="{{asset('admin')}}/img/AdminLTELogo.png" alt="Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Dashboard</span>
     </a>
@@ -60,7 +60,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{asset('admin')}}/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset('admin')}}/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User">
         </div>
         <div class="info">
           <a href="{{ route('user.profile') }}" class="d-block">{{ Auth::user()->name }}</a>
